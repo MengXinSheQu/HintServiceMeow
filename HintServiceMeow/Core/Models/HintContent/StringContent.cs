a1678a8060d1d516ad31a3a96f94f9adcdd5458e
史蒂夫 <steam114514@126.com>
2025-07-24 13:14:08 +0800

    fix lower/upper

--- HintServiceMeow/Core/Models/HintContent/StringContent.cs
+++ HintServiceMeow/Core/Models/HintContent/StringContent.cs
@@ -3,7 +3,27 @@
 namespace HintServiceMeow.Core.Models.HintContent
 {
     public class StringContent : AbstractHintContent
-    {
+    {       
+        private static readonly Regex CoreRegex = new("(<.*?>)");
+        
+        private static string AddLower(string old)
+        {
+            var s = CoreRegex.Split(old);
+            string txt = "";
+            foreach (var i in s)
+            {
+                if (CoreRegex.IsMatch(i)) txt += i;
+                else
+                    foreach (var p in i)
+                    {
+                        if (IsUpper(p))
+                            txt += $"<uppercase>{p}</uppercase>";
+                        else
+                            txt += p;
+                    }
+            }
+            return $"<lowercase>{txt}</lowercase>";
+        }
         private string _text = string.Empty;
 
         public string Text
@@ -25,7 +45,7 @@ namespace HintServiceMeow.Core.Models.HintContent
             this.Text = content;
         }
 
-        public override string GetText() => Text;
+        public override string GetText() => AddLower(Text);
 
         public override void TryUpdate(ContentUpdateArg ev) { }
     }
